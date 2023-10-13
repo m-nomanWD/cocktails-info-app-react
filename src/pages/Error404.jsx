407fdaf9c353a4deb5fe690bfe1cb02eae7cabc9
muhammad noman <mnomanwd@gmail.com>
2023-10-12 22:50:46 -0700

    error page
    error 404 page is complete

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,8 +1,37 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export class Error404 extends Component {
   render() {
-    return <div>Error404</div>
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '70vh',
+          color: 'brown',
+        }}
+      >
+        <h1>Sorry Page not Found Error 404 </h1>
+        <Link
+          to={'/'}
+          className='button-container'
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textDecoration: 'none',
+            marginTop: '2rem',
+          }}
+        >
+          <button style={{ width: '140px' }} className='detail-btn'>
+            Home Page
+          </button>
+        </Link>
+      </div>
+    )
   }
 }
 
